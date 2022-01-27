a5e5fb0c71376c8ad785aa5ec301e8247aaf7acb
martin307 <martin307@schule.bremen.de>
2022-01-27 15:15:14 +0100

    Fusball verzweiflung #2

--- Fußball/src/schritt1/Programm.java
+++ Fußball/src/schritt1/Programm.java
@@ -1,18 +1,50 @@
 package schritt1;
 
 import schritt2.Torwart;
+import schritt4.Mannschaft;
+
+import java.util.ArrayList;
 
 public class Programm {
 
     public static void main(String[] args) {
 
-        Trainer trainer = new Trainer("\tUwe", 50, 6);
-        Spieler spieler = new Spieler("\tWerner", 25, 30, 14, 5, 2);
-        Torwart torwart = new Torwart("\t Ulf", 22, 50,5,0);
 
-        System.out.println(trainer);
-        System.out.println(spieler);
-        System.out.println(torwart);
+
+        Trainer heimtrainer = new Trainer("Jürgen Windhose", 49, 6);
+        Torwart heimtorwart = new Torwart("Jürgen Wanne", 24, 6, 4, 6);
+
+
+        Spieler sp1 = new Spieler("Olaf", 21, 8, 2, 6, 0);
+        Spieler sp2 = new Spieler("Dieter", 27, 3, 8, 3, 4);
+        Spieler sp3 = new Spieler("Gustav", 25, 4, 4, 4, 1);
+        Spieler sp4 = new Spieler("Korn", 20, 3, 5, 9, 3);
+
+        ArrayList<Spieler> heimspielerliste = new ArrayList<>();
+        heimspielerliste.add(sp1);
+        heimspielerliste.add(sp2);
+        heimspielerliste.add(sp3);
+        heimspielerliste.add(sp4);
+
+        Mannschaft heim = new Mannschaft("Heim", heimtrainer, heimtorwart, heimspielerliste);
+
+        Trainer gasttrainer = new Trainer("Uwe Schelle", 60, 10);
+        Torwart gasttorwart = new Torwart("Ulf Tatort", 12, 10, 0, 2);
+
+
+        Spieler spA = new Spieler("Paul", 21, 2, 8, 0, 6);
+        Spieler spB = new Spieler("Karl", 27, 5, 1, 5, 4);
+        Spieler spC = new Spieler("Flensburg", 25, 3, 6, 6, 1);
+        Spieler spD = new Spieler("Schnaps", 20, 3, 5, 7, 3);
+
+        ArrayList<Spieler> gastspielerliste = new ArrayList<>();
+        gastspielerliste.add(spA);
+        gastspielerliste.add(spB);
+        gastspielerliste.add(spC);
+        gastspielerliste.add(spD);
+
+        Mannschaft gast = new Mannschaft("Gast", gasttrainer, gasttorwart, gastspielerliste);
+
 
     }
 }
--- Fußball/src/schritt4/Mannschaft.java
+++ Fußball/src/schritt4/Mannschaft.java
@@ -21,43 +21,6 @@ public class Mannschaft {
         this.spielerliste = spielerliste;
     }
 
-    //Heimmanschaft
-
-    Trainer heimtrainer = new Trainer("Jürgen Windhose", 49, 6);
-    Torwart heimtorwart = new Torwart("Jürgen Wanne", 24, 6, 4, 6);
-
-
-    Spieler sp1 = new Spieler("Olaf", 21, 8, 2, 6, 0);
-    Spieler sp2 = new Spieler("Dieter", 27, 3, 8, 3, 4);
-    Spieler sp3 = new Spieler("Gustav", 25, 4, 4, 4, 1);
-    Spieler sp4 = new Spieler("Korn", 20, 3, 5, 9, 3);
-
-    ArrayList<Spieler> heimspielerliste = new ArrayList<>();
-    heimspielerliste.add(sp1);
-    heimspielerliste.add(sp2);
-    heimspielerliste.add(sp3);
-    heimspielerliste.add(sp4);
-
-    Mannschaft heim = new Mannschaft("Heim", heimtrainer, heimtorwart, heimspielerliste);
-
-    //Gastmanschaft
-
-    Trainer gasttrainer = new Trainer("Uwe Schelle", 60, 10);
-    Torwart gasttorwart = new Torwart("Ulf Tatort", 12, 10, 0, 2);
-
-
-    Spieler spA = new Spieler("Paul", 21, 2, 8, 0, 6);
-    Spieler spB = new Spieler("Karl", 27, 5, 1, 5, 4);
-    Spieler spC = new Spieler("Flensburg", 25, 3, 6, 6, 1);
-    Spieler spD = new Spieler("Schnaps", 20, 3, 5, 7, 3);
-
-    ArrayList<Spieler> gastspielerliste = new ArrayList<>();
-    gastspielerliste.add(spA);
-    gastspielerliste.add(spB);
-    gastspielerliste.add(spC);
-    gastspielerliste.add(spD);
-
-    Mannschaft gast = new Mannschaft("Gast", gasttrainer, gasttorwart, gastspielerliste);
 
     //Getter/Setter
 
@@ -86,32 +49,39 @@ public class Mannschaft {
         this.torwart = torwart;
     }
 
-    public ArrayList<Spieler> getHeimspielerliste() {
-        return heimspielerliste;
-    }
-
-    public ArrayList<Spieler> getGastspielerliste() {
-        return gastspielerliste;
-    }
-
     public ArrayList<Spieler> getSpielerliste() {
         return spielerliste;
-    }
-
-    public void setGastspielerliste(ArrayList<Spieler> gastspielerliste) {
-        this.gastspielerliste = gastspielerliste;
-    }
-    public void setHeimspielerliste(ArrayList<Spieler> heimspielerliste){
-        this.heimspielerliste = heimspielerliste;
     }
 
     public void setSpielerliste(ArrayList<Spieler> spielerliste) {
         this.spielerliste = spielerliste;
     }
+
+
     //Motivation
 
+    public int getMotivation() {
+        int motivation = 0;
+        for (Spieler s : spielerliste) {
+            motivation = motivation + s.getMotivation();
+        }
+        motivation = motivation + torwart.getMotivation();
+        motivation = motivation / spielerliste.size();
+
+        return motivation;
+    }
 
     //Stärke
 
+    public int getStaerke() {
+        int staerke = 0;
+        for (Spieler s : spielerliste) {
+            staerke = staerke + s.getStaerke();
+        }
+        staerke = staerke + torwart.getStaerke();
+        staerke = staerke / spielerliste.size();
 
+        return staerke;
+
+    }
 }
