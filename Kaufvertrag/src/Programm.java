7a47e68c07ffe3f1c4482025f082d8867477fdc8
martin307 <martin307@schule.bremen.de>
2022-03-17 15:22:00 +0100

    just no

--- Kaufvertrag/src/Programm.java
+++ Kaufvertrag/src/Programm.java
@@ -9,6 +9,29 @@ public class Programm {
         writer.close();
     }
 
+    public static void printCsv(Kaufvertrag vertrag) throws FileNotFoundException {
+
+        String datei = "H:/LF02/Printwriter/Kaufvertrag.csv";
+        PrintWriter printWriter = new PrintWriter(datei);
+
+        printWriter.println("Vertragspartner ;" + "Vorname" + " " + "Nachname ; " + "Strasse" + " " + "Hausnummer"
+                + ";" + "Postleitzahl" + " " + "Ort");
+
+        printWriter.print("Kaeufer ; ");
+        printWriter.print(vertrag.getKaeufer().getVorname() + " " + vertrag.getKaeufer().getNachname() + ";");
+        printWriter.println(vertrag.getKaeufer().getAdresse().getStrasse() + " "
+                + vertrag.getKaeufer().getAdresse().getHausNr() + ";" + vertrag.getKaeufer().getAdresse().getPlz()
+                + " " + vertrag.getKaeufer().getAdresse().getOrt());
+
+        printWriter.print("Verkaeufer ;");
+        printWriter.print(vertrag.getVerkaeufer().getVorname() + " " + vertrag.getVerkaeufer().getNachname() + ";");
+        printWriter.println(vertrag.getVerkaeufer().getAdresse().getStrasse() + " "
+                + vertrag.getVerkaeufer().getAdresse().getHausNr() + ";" + vertrag.getVerkaeufer().getAdresse().getPlz()
+                + " " + vertrag.getVerkaeufer().getAdresse().getOrt());
+
+        printWriter.close();
+    }
+
 
     public static void main(String[] args) throws IOException {
         Ware ware = new Ware("Laptop", 250.0);
@@ -20,16 +43,17 @@ public class Programm {
 
         Vertragspartner kaeufer = new Vertragspartner("Klaus", "Brandt");
         kaeufer.setAusweisNr("0123456789");
-        kaeufer.setAdresse(new Adresse("Zu Hause", "3a", "28199", "Bremen"));
+        kaeufer.setAdresse(new Adresse("Wurstallee", "23a", "28199", "Bremen"));
 
         Vertragspartner verkaeufer = new Vertragspartner("Joachim", "Bredull");
         verkaeufer.setAusweisNr("9876543210");
-        verkaeufer.setAdresse(new Adresse("Auch zu Hause", "7", "28195", "Bremen"));
+        verkaeufer.setAdresse(new Adresse("Entemhausen", "756", "28195", "Bremen"));
 
         Kaufvertrag vertrag = new Kaufvertrag(verkaeufer, kaeufer, ware);
         vertrag.setZahlungsModalitaeten("Privater Barverkauf");
 
         printWriter(vertrag);
+        printCsv(vertrag);
 
 
     }
--- Kaufvertrag/src/ReadCsv.java
+++ Kaufvertrag/src/ReadCsv.java
@@ -1,17 +1,48 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReadCsv {
 
-    public static void readCsv() throws IOException {
+    public static void main(String[] args) throws IOException {
+
         String datei = "H:/LF02/Printwriter/Kaufvertrag.csv";
         BufferedReader br = new BufferedReader(new FileReader(datei));
 
         String zeile;
         while ((zeile = br.readLine()) != null) {
 
-        }
+            String[] datensatz = zeile.split(";");
 
+            if (datensatz[0].equalsIgnoreCase("kaeufer")) {
+                String[] name = datensatz[1].split(" ");
+                Vertragspartner kaeufer = new Vertragspartner(name[0], name[1]);
+                String[] adresse = datensatz[2].split(" ");
+                String[] adresse1 = datensatz[3].split(" ");
+
+                String strasse = adresse[0];
+                String hausNr = adresse[adresse.length - 1];
+                String plz = adresse1[0];
+                String ort = adresse1[1];
+                Adresse adressekaeufer = new Adresse(strasse, hausNr, plz, ort);
+
+            }
+            if (datensatz[0].equalsIgnoreCase("verkaeufer")) {
+                String[] name = datensatz[1].split(" ");
+                Vertragspartner verkaeufer = new Vertragspartner(name[0], name[1]);
+                String[] adresse = datensatz[2].split(" ");
+                String[] adresse1 = datensatz[3].split(" ");
+
+                String strasse = adresse[0];
+                String hausNr = adresse[adresse.length - 1];
+                String plz = adresse1[0];
+                String ort = adresse1[1];
+                Adresse adresseverkaeufer = new Adresse(strasse, hausNr, plz, ort);
+
+
+            }
+
+
+        }
     }
-}
