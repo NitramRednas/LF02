6c2dbe076d68bb7315fce63dc81635501c8c5d46
martin307 <martin307@schule.bremen.de>
2022-03-21 11:11:20 +0100

    just no#3 Pdf edition

--- Kaufvertrag/src/KaufvertragPdf.java
+++ Kaufvertrag/src/KaufvertragPdf.java
@@ -1,16 +1,71 @@
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.property.TextAlignment;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class KaufvertragPdf {
 
-    public static void PdfWriter(Kaufvertrag kaufvertrag) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+
+    Kaufvertrag kaufvertrag = schreibenKaufvertrag();
+    pdfWriter(kaufvertrag);
+
+
+    }
+
+    public static void pdfWriter(Kaufvertrag kaufvertrag) throws IOException {
 
         String datei = "H:/LF02/Printwriter/Kaufvertrag.pdf";
         PdfWriter writer = new PdfWriter(datei);
         PdfDocument pdfDocument = new PdfDocument(writer);
         Document document = new Document(pdfDocument);
 
+        PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+        document.setFont(font);
+        document.setFontSize(20);
+        document.setBold();
+        document.setItalic();
+        document.setUnderline();
+        document.setTextAlignment(TextAlignment.LEFT);
+        document.setFontColor(new DeviceRgb(50,100,150));
+
 
         document.close();
+        }
+
+    public static Kaufvertrag schreibenKaufvertrag() {
+        Ware ware = new Ware("Laptop", 250.0);
+        ware.setBeschreibung("Super krasses Gaming-Laptop");
+        ware.getBesonderheitenListe().add("Tasche");
+        ware.getBesonderheitenListe().add("Gaming-Maus");
+        ware.getMaengelListe().add("Ladekabel fehlt");
+        ware.getMaengelListe().add("Touchpad defekt");
+
+        Vertragspartner kaeufer = new Vertragspartner("Klaus", "Brandt");
+        kaeufer.setAusweisNr("0123456789");
+        kaeufer.setAdresse(new Adresse("Wurstallee", "23a", "28199", "Bremen"));
+
+        Vertragspartner verkaeufer = new Vertragspartner("Joachim", "Bredull");
+        verkaeufer.setAusweisNr("9876543210");
+        verkaeufer.setAdresse(new Adresse("Entemhausen", "756", "28195", "Bremen"));
+
+        Kaufvertrag vertrag = new Kaufvertrag(verkaeufer, kaeufer, ware);
+        vertrag.setZahlungsModalitaeten("Privater Barverkauf");
+
+        System.out.println(vertrag);
+        return vertrag;
+
 
     }
+
+
+
 }
