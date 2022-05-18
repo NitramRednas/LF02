ef9af35b162487960ae1aa8d0a905fd592ae485d
martin307 <martin307@schule.bremen.de>
2022-05-18 12:18:42 +0200

    Weeeeeeeee

--- SQL to Java/src/dao/Programm.java
+++ SQL to Java/src/dao/Programm.java
@@ -1,5 +1,6 @@
 package dao;
 
+import bussinesObjects.Ware;
 import bussinesObjects.Vertragspartner;
 
 public class Programm {
@@ -7,6 +8,9 @@ public class Programm {
     public static void main(String[] args) throws ClassNotFoundException{
 
         WareDao wareDao = new WareDao();
+        Ware ware = wareDao.read("1");
+        System.out.println(ware.getBeschreibung());
+
         VertragspartnerDao vertragspartnerDao = new VertragspartnerDao();
         Vertragspartner vertragspartner = vertragspartnerDao.read("123");
         System.out.println(vertragspartner.getNachname());
--- SQL to Java/src/dao/WareDao.java
+++ SQL to Java/src/dao/WareDao.java
@@ -1,5 +1,9 @@
 package dao;
 
+import bussinesObjects.Ware;
+
+import java.sql.*;
+
 public class WareDao {
 
     private final String CLASSNAME = "org.sqlite.JDBC";
@@ -10,4 +14,103 @@ public class WareDao {
 
     }
 
+    /**
+
+     * Liest eine Ware auf basis seiner Warennummer
+
+     * @param warenNr Die Ausweisnummer
+
+     * @return Der Gewünschte Vertragspartner
+
+     */
+
+
+
+    public Ware read(String warenNr){
+
+        Ware ware = null;
+
+        Connection connection = null;
+
+        PreparedStatement preparedStatement = null;
+
+
+
+        // Verbindung zu Datenbank Herstellen
+
+
+
+        try {
+
+            connection = DriverManager.getConnection(CONNECTIONSTRING);
+
+
+
+            //SQL-Abfrage erstellen
+
+            String sql = "SELECT * From Ware WHERE warenNr = ?";
+
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1,warenNr);
+
+
+
+            //SQL-Abfrage ausführen
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+
+            //Zeiger auf den ersten Datensatz setzen
+
+            resultSet.next();
+
+
+
+            //ResulSet auswerten
+
+            String Nr = resultSet.getString("warenNr");
+            String bezeichnung = resultSet.getString("bezeichnung");
+            String beschreibung = resultSet.getString("beschreibung");
+            double preis = resultSet.getDouble("preis");
+            String besonderheiten = resultSet.getString("besonderheiten");
+            String maengel = resultSet.getString("maengel");
+
+
+            //Vertragsprtner ertselle
+
+            ware = new Ware(bezeichnung,preis);
+
+            ware.setBezeichnung(bezeichnung);
+
+            ware.setPreis(preis);
+
+            ware.setBeschreibung(beschreibung);
+
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }finally {
+
+            try {
+
+                connection.close();
+
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+        return ware;
+
+    }
+
 }
+
+
